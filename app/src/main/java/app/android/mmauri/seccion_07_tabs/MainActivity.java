f93ea1206a17cc8a0edbf92cd3c178e666b80b9d
Marc from GitKraken <marcmauri92@gmail.com>
2017-11-08 12:50:10 +0100

    Implementado Toolbar basico

--- app/src/main/java/app/android/mmauri/seccion_07_tabs/MainActivity.java
+++ app/src/main/java/app/android/mmauri/seccion_07_tabs/MainActivity.java
@@ -2,6 +2,10 @@ package app.android.mmauri.seccion_07_tabs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.exit:
+                Toast.makeText(MainActivity.this, "Exit button", Toast.LENGTH_SHORT).show();
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
     }
 }
